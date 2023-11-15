92061dab38072eddc234f0ce1df772ae1baba950
zeinabjalali <jalalizeinab95@gmail.cm>
2023-11-15 10:50:38 +0330

    improvements improve packaging and function splitting improve MovieDetail.kt for adding nullable to posterPath

---
+++ app/src/main/java/com/example/tmdb/feature/detail/ui/components/SimilarMovies.kt
@@ -0,0 +1,119 @@
+package com.example.tmdb.feature.detail.ui.components
+
+
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.basicMarquee
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import com.example.tmdb.R
+import com.example.tmdb.core.ui.theme.designsystem.TMDBTheme
+import com.example.tmdb.feature.detail.network.json.MovieDetail
+import com.example.tmdb.feature.detail.network.json.SimilarMovieResult
+import com.example.tmdb.feature.detail.ui.common.RowWithIconAndText
+import com.example.tmdb.feature.detail.ui.imageUrl
+import java.math.RoundingMode
+
+@Composable
+@OptIn(ExperimentalFoundationApi::class)
+fun SimilarMovies(movieDetail: MovieDetail) {
+    if (movieDetail.similar.results.isNotEmpty()) {
+        Text(
+            text = stringResource(R.string.similar_movies),
+            style = TMDBTheme.typography.subtitle1,
+            color = TMDBTheme.colors.white,
+            modifier = Modifier
+                .padding(
+                    top = 24.dp,
+                    start = 24.dp
+                )
+        )
+
+        LazyRow(
+            contentPadding = PaddingValues(
+                start = 24.dp,
+                top = 16.dp
+            )
+        ) {
+            items(movieDetail.similar.results) { similarMovie ->
+                Column {
+
+                    PosterWithTotalVote(similarMovie)
+
+                    Text(
+                        text = similarMovie.originalTitle,
+                        style = TMDBTheme.typography.body1,
+                        color = TMDBTheme.colors.white,
+                        modifier = Modifier
+                            .widthIn(60.dp, 135.dp)
+                            .padding(horizontal = 8.dp)
+                            .padding(top = 12.dp, bottom = 4.dp)
+                            .basicMarquee()
+                    )
+                    Text(
+                        text = similarMovie.genreIds[0].toString(),
+                        style = TMDBTheme.typography.overLine,
+                        color = TMDBTheme.colors.gray,
+                        modifier = Modifier.padding(start = 8.dp)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun PosterWithTotalVote(similarMovie: SimilarMovieResult) {
+    Box {
+        AsyncImage(
+            model = "$imageUrl${similarMovie.posterPath}",
+            contentDescription = null,
+            contentScale = ContentScale.Fit,
+            modifier = Modifier
+                .clip(
+                    TMDBTheme.shapes.medium.copy(
+                        bottomEnd = CornerSize(0.dp),
+                        bottomStart = CornerSize(0.dp)
+                    )
+                )
+                .width(135.dp)
+                .aspectRatio(0.7f),
+            error = painterResource(id = R.drawable.videoimageerror)
+        )
+        Row(
+            modifier = Modifier
+                .align(Alignment.TopEnd)
+                .padding(8.dp)
+                .clip(TMDBTheme.shapes.small)
+                .background(TMDBTheme.colors.surface.copy(alpha = 0.7f))
+        ) {
+            val roundedVote = similarMovie.voteAverage.toBigDecimal()
+                .setScale(1, RoundingMode.FLOOR).toDouble()
+            RowWithIconAndText(
+                text = roundedVote.toString(),
+                iconId = R.drawable.star,
+                iconColor = TMDBTheme.colors.secondary,
+                textColor = TMDBTheme.colors.secondary
+            )
+        }
+    }
+}
