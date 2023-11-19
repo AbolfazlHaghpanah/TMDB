af6dbe904640c3cfeec85246103d17c3a19f539f
Abolfazl Haghpanah <abolfazlhaghpanah77@gmail.com>
2023-11-19 15:16:21 +0330

    implement Splash Screen -add launcher icon -add splashscreen api

--- app/src/main/java/com/example/tmdb/MainActivity.kt
+++ app/src/main/java/com/example/tmdb/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.material.rememberScaffoldState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
@@ -32,6 +33,7 @@ class MainActivity : ComponentActivity() {
     @OptIn(ExperimentalMaterialNavigationApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         enableEdgeToEdge(
             statusBarStyle = SystemBarStyle.dark(
                 scrim = Color.Transparent.toArgb()
