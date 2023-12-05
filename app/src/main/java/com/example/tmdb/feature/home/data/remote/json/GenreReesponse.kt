3fc2de09ac44752569c5486b28877df4866fdb1b
zeinabjalali <jalalizeinab95@gmail.cm>
2023-12-05 08:52:27 +0330

    added review

--- app/src/main/java/com/example/tmdb/feature/home/data/remote/json/GenreReesponse.kt
+++ app/src/main/java/com/example/tmdb/feature/home/data/remote/json/GenreReesponse.kt
@@ -3,16 +3,17 @@ package com.example.tmdb.feature.home.data.remote.json
 import com.example.tmdb.core.data.genre.entity.GenreEntity
 import kotlinx.serialization.Serializable
 
+//TODO rename to xDto -> GenreResponseDto
 @Serializable
 data class GenreResponse(
-    val genres : List<GenresResponse>
+    val genres: List<GenresResponse>
 )
 
 @Serializable
 data class GenresResponse(
-    val id : Int,
-    val name : String
-){
+    val id: Int,
+    val name: String
+) {
     fun toGenreEntity(): GenreEntity {
         return GenreEntity(
             genreId = id,
