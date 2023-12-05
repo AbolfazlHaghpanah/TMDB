7bb350a603d6740ddd7b7a719197c382d9227d32
zeinabjalali <jalalizeinab95@gmail.cm>
2023-12-05 08:53:48 +0330

    removed todo

--- app/src/main/java/com/example/tmdb/feature/home/data/remote/json/GenreReesponse.kt
+++ app/src/main/java/com/example/tmdb/feature/home/data/remote/json/GenreReesponse.kt
@@ -3,7 +3,6 @@ package com.example.tmdb.feature.home.data.remote.json
 import com.example.tmdb.core.data.genre.entity.GenreEntity
 import kotlinx.serialization.Serializable
 
-//TODO rename to xDto -> GenreResponseDto
 @Serializable
 data class GenreResponse(
     val genres: List<GenresResponse>
