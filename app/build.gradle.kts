822fe3e1604ccb9131d568af4fb429b47480ebb8
Abolfazl Haghpanah <abolfazlhaghpanah77@gmail.com>
2023-12-26 17:14:22 +0330

    remove unused dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,18 +57,13 @@ android {
 
 dependencies {
 
+    //compose
     implementation(libs.core.ktx)
     implementation(libs.activity.compose)
     implementation(libs.ui)
     implementation(libs.ui.graphics)
     implementation(libs.ui.tooling.preview)
     implementation(libs.material)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.test.ext.junit)
-    androidTestImplementation(libs.espresso.core)
-    androidTestImplementation(libs.ui.test.junit4)
-    debugImplementation(libs.ui.tooling)
-    debugImplementation(libs.ui.test.manifest)
 
     //lifecycle
     implementation(libs.bundles.lifecycle)
