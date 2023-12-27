888fd76c92efc9f432c84abd5748f5743857c9c4
zeinabjalali <jalalizeinab95@gmail.cm>
2023-12-27 16:26:24 +0330

    added @Singleton annotation of Dispatcher provider

--- app/src/main/java/com/hooshang/tmdb/core/di/DispatcherModule.kt
+++ app/src/main/java/com/hooshang/tmdb/core/di/DispatcherModule.kt
@@ -5,10 +5,12 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import kotlinx.coroutines.Dispatchers
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 class DispatcherModule {
+    @Singleton
     @Provides
     fun provideDispatcher(): Dispatchers = Dispatchers
 }
