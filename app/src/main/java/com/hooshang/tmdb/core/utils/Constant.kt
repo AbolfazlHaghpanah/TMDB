d1a2eb483d1cc257f6541568bf1410adcb7b2f57
Abolfazl Haghpanah <abolfazlhaghpanah77@gmail.com>
2024-06-07 10:56:55 +0330

    update versions

--- app/src/main/java/com/hooshang/tmdb/core/utils/Constant.kt
+++ app/src/main/java/com/hooshang/tmdb/core/utils/Constant.kt
@@ -1,8 +1,7 @@
 package com.hooshang.tmdb.core.utils
 
 
-const val tmdb_api_key =
-    "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxYWQ0YTgxZGZlOWY1ODlhMTIwZTIwNzE5MmFlY2E3MSIsInN1YiI6IjY1NGIyYTU0MzkxYjljMDExZDMwNWI2MiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Pf8Q1FtyfF0qoH2H_YvdiBhxsFUUwEKnQ0cG1dKCe2Q"
+const val tmdb_api_key = "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxYWQ0YTgxZGZlOWY1ODlhMTIwZTIwNzE5MmFlY2E3MSIsInN1YiI6IjY1NGIyYTU0MzkxYjljMDExZDMwNWI2MiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Pf8Q1FtyfF0qoH2H_YvdiBhxsFUUwEKnQ0cG1dKCe2Q"
 const val image_url = "https://tmdb-api.samentic.com/image/t/p/w500/"
 const val api_version = "3"
 const val base_url = "https://api.themoviedb.org/$api_version/"
